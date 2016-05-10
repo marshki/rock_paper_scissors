093fe6dd3a21fa2bec6944f9305ab813287baae2
marshki <marshall@morethantomorrow.org>
2016-05-10 12:02:58 -0400

    Add comments. Remove whitespace.

--- rock_scissor_paper.py
+++ rock_scissor_paper.py
@@ -4,7 +4,6 @@
 """A text-based Python 2.7.6 implementation of the game Rock-Paper-Scissors.
 
 Game play:  
-
         * each round, both players select from: rock, paper, or scissors 
         * rock beats scissors, scissors beats paper, paper beats rock 
         * a player receives 1 point per round (s)he wins
@@ -29,7 +28,6 @@ print"""
 See if you can win five rounds before the bot.
 
 Keep in mind:
-
         * rock breaks scissors,  
         * scissors cut paper, 
         * paper covers rock. 
@@ -42,7 +40,7 @@ def human():	# Prompt for user input. Accept upper and lower variants
 	print 
 	print "R: Rock    P: Paper    S: Scissor" 
 	human_move = raw_input("Enter your choice: ").lower()
-	return human_move
+	return human_move 
 ()
 
 def bot():	# Randomize bot's move 
@@ -50,7 +48,7 @@ def bot():	# Randomize bot's move
 	return bot_move
 ()
 
-def game_over():	# Game over when either player scores five 
+def game_over():	# Game over when either player scores five points 
 	if bot_score == 5: 
 		print 
 		print "The bot wins, you puny human. " 
@@ -62,7 +60,6 @@ def game_over():	# Game over when either player scores five
 ()
 
 # The program 
-
 while True:	# While loop to compare output of players, decide on winner, and keep score 
 	hu, bo  = human(), bot()
 	
