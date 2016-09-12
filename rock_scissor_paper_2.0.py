01551cfde44cc4f5d01d7d7bd06f83cfe660ed5e
marshki <marshall@morethantomorrow.org>
2016-09-12 09:47:45 -0400

    Changes to address landscape.io

--- rock_scissor_paper_2.0.py
+++ rock_scissor_paper_2.0.py
@@ -1,28 +1,28 @@
-from __future__ import print_function 			
+from __future__ import print_function 
 from builtins import input				 
 
 #!/bin/py 
 #Python 2 & 3 
 
-"""
+""" 
 A text-based Python 2 & 3 implementation of the game Rock-Paper-Scissors.
 Game play:  
         * each round, both players select from: rock, paper, or scissors 
         * rock beats scissors, scissors beats paper, paper beats rock 
         * a player receives 1 point per round (s)he wins
         * 0 points for ties 
-        * first player to win user designated number of rounds is the victor 
+        * first player to win user designated number of rounds is the victor
 """
 
 # Import libraries  
 
-from random import choice 				# to randomize bot moves 
+from random import choice                               # to randomize bot moves 
 
 # Declarations 
 
-MOVES = ["r", "p", "s"] 				# Possible moves  
+MOVES = ["r", "p", "s"]                                 # Possible moves  
 
-WINNING_COMBOS = {("r", "s"), ("s", "p"), ("p", "r")} 	# Winning outcomes 
+WINNING_COMBOS = {("r", "s"), ("s", "p"), ("p", "r")}   # Winning outcomes 
 
 # Define functions 
 
@@ -51,23 +51,23 @@ def human():
 
 
 def bot():	
-	""" Randomize bot's move.""" 
-	bot_move = choice(MOVES)					
-	return bot_move
+    """ Randomize bot's move.""" 
+    bot_move = choice(MOVES)					
+    return bot_move
 
 
 def play_round():
-    '''play a single round and either return the winner's name or 'q' to retire'''
-    while True:					# while loop to compare output of players, decide on winner, and keep score 
+    """Play a single round and either return the winner's name or 'q' to retire"""
+    while True:                             # while loop to compare output of players, decide on winner, and keep score 
         hu, bo  = human(), bot()
-        if hu == "q":				# Allow user to quit at anytime
+        if hu == "q":                       # Allow user to quit at anytime
             return hu
-        if  hu == bo:				# conditions for a tie  
+        if  hu == bo:                       # conditions for a tie  
             print("You both choose %s. Grr... a tie!" % (bo))
-        elif (hu, bo) in WINNING_COMBOS:	# conditions for user win 
+        elif (hu, bo) in WINNING_COMBOS:    # conditions for user win 
             print("You picked %s and the bot picked %s. Woo-hoo! You win this one, human." %(hu, bo))
             return 'human'
-        else:					# conditions for bot win 
+        else:					            # conditions for bot win 
             print("You picked %s and the bot picked %s. Bwahahaha! The almighty bot wins!" %(hu, bo))
             return 'bot'
 
@@ -78,27 +78,27 @@ def play():
 ********** Welcome to Rock-Paper-Scissors! **********
  See if you can beat the bot!
  Keep in mind:
- 	* rock breaks scissors,
+ 	    * rock breaks scissors,
         * scissors cut paper,
         * paper covers rock.
 Good luck! 
 	""")
 
-    game_length = rounds()							# number of rounds 
-    scores = {'human':0, 'bot':0}						# initialize scores 
+    game_length = rounds()                              # number of rounds 
+    scores = {'human':0, 'bot':0}                       # initialize scores 
     while True:							
         result = play_round()
-        if result == 'q':							# allow user to end game 
+        if result == 'q':                               # allow user to end game 
             break
 
-        scores[result] += 1							# update score 
+        scores[result] += 1                             # update score 
         print("Your score: {human}, Bot score: {bot}".format(**scores))
-        if scores['bot'] == game_length:					# bot wins == user defined number of rounds  
+        if scores['bot'] == game_length:                # bot wins == user defined number of rounds  
             print("The bot wins, you puny human. " )
             break
-        elif scores['human'] == game_length: 					# human wins == user defined number of rounds 
+        elif scores['human'] == game_length: 			# human wins == user defined number of rounds 
             print("The puny human wins." )
             break
 
-if __name__ == '__main__':							# let the games begin 
+if __name__ == '__main__':                              # let the games begin 
     play()
