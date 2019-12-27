b4833bbbe2a824c3c3808cdf9e2a0219144263fb
marshki <marshall@morethantomorrow.org>
2019-12-27 12:41:32 -0500

    fix pylint errors

--- tests/rounds.py
+++ tests/rounds.py
@@ -1,18 +1,23 @@
-from __future__ import print_function 
-from builtins import input 
+#!/usr/bin/env python
 
-#!/usr/bin/env python 
+"""Unit test.
+"""
+
+from __future__ import print_function
+from builtins import input
 
 def rounds():
-    """Prompt user for rounds of play"""
+    """Prompt user.
+    Return number of rounds as integer.
+    """
     while True:
         try:
             num_rounds = int(input("Type the number of rounds to play and press enter: "))
-            print ("O.K. Let\'s play {} round(s)".format(num_rounds))
+            print("O.K. Let\'s play {} round(s)".format(num_rounds))
             return num_rounds
 
         except ValueError:
             print("Sorry, that\'s invalid input. Try again: ")
 
 if __name__ == '__main__':
-	rounds()
+    rounds()
