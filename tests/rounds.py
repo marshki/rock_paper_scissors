7b22a65f8f58d6f3e777bfe03923a86bbf919494
marshki <marshall@morethantomorrow.org>
2019-12-27 13:20:07 -0500

    add

--- tests/rounds.py
+++ tests/rounds.py
@@ -6,6 +6,10 @@
 from __future__ import print_function
 from builtins import input
 
+import unittest
+
+from unittest.mock import patch
+
 def rounds():
     """Prompt user.
     Return number of rounds as integer.
@@ -19,5 +23,25 @@ def rounds():
         except ValueError:
             print("Sorry, that\'s invalid input. Try again: ")
 
+class Rounds(unittest.TestCase):
+
+    """Unit tests.
+    Use `patch()` to mock objects for testing.
+    For reference: https://docs.python.org/3/library/unittest.mock.html
+    """
+
+    @patch('builtins.input', return_value='10')
+    def test_rounds_01(self, input):
+        """Valid return value.
+        """
+        self.assertIsInstance(rounds(), int)
+
+    @patch('builtins.input', return_value='Derp!')
+    def test_rounds_02(self, input):
+        """Invalid return value.
+        """
+        with self.assertRaises(ValueError):
+            rounds()
+
 if __name__ == '__main__':
-    rounds()
+    unittest.main()
