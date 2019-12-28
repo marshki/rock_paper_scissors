c7ae05aef15be1057045dd118777e4d1e651bdf0
marshki <marshall@morethantomorrow.org>
2019-12-28 10:05:14 -0500

    update set

--- tests/player2.py
+++ tests/player2.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
+# pylint: disable=W0613,W0622
 
-"""Unit test suite.
+"""Unit testing class.
 """
 
+import unittest
+from unittest.mock import patch
 from random import choice
 
 MOVES = ["r", "p", "s"]
@@ -14,5 +17,23 @@ def player2():
 
     return player2_move
 
+class Player2(unittest.TestCase):
+    """Unit tests.
+    Use `patch()` to mock objects for testing.
+    For reference: https://docs.python.org/3/library/unittest.mock.html
+    """
+
+    @patch('builtins.input', return_value='r')
+    def test_player2_01(self, input):
+        """Valid return value.
+        """
+        self.assertIn(player2(), ['r', 'p', 's'])
+
+    @patch('builtins.input', return_value='s')
+    def test_player2_02(self, input):
+        """Valid return value.
+        """
+        self.assertIn(player2(), ['r', 'p', 's'])
+
 if __name__ == '__main__':
-    player2()
+    unittest.main()
