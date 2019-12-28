c63a62f641a09559aaafa848ff4f4cedd54af1a6
marshki <marshall@morethantomorrow.org>
2019-12-27 20:37:56 -0500

    add unit test (works!)

--- tests/rounds.py
+++ tests/rounds.py
@@ -1,27 +1,22 @@
 #!/usr/bin/env python
 
-"""Unit test.
+"""Unit testing class.
 """
 
-from __future__ import print_function
-from builtins import input
-
 import unittest
-
 from unittest.mock import patch
 
 def rounds():
     """Prompt user.
     Return number of rounds as integer.
     """
+
     while True:
         try:
-            num_rounds = int(input("Type the number of rounds to play and press enter: "))
-            print("O.K. Let\'s play {} round(s)".format(num_rounds))
-            return num_rounds
-
+            return int(input("Type the number of rounds to play and press enter: "))
         except ValueError:
             print("Sorry, that\'s invalid input. Try again: ")
+            raise
 
 class Rounds(unittest.TestCase):
 
