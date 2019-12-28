c9cfc26cfb276ab4ffa665d89744f5e394a82433
marshki <marshall@morethantomorrow.org>
2019-12-27 20:56:54 -0500

    add

--- tests/player2.py
+++ tests/player2.py
@@ -1,17 +1,18 @@
-from __future__ import print_function
-from builtins import input
-
-
 #!/usr/bin/env python
+
+"""Unit test suite.
+"""
 
 from random import choice
 
 MOVES = ["r", "p", "s"]
 
 def player2():
-	"""Randomize bot's move."""
-	player2_move = choice(MOVES)
-	return player2_move 
+    """Randomize bot's move.
+    """
+    player2_move = choice(MOVES)
+
+    return player2_move
 
 if __name__ == '__main__':
-	player2()
+    player2()
