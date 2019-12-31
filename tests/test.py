44a55ea455d745cb6ff937f16994f9ed6d52a65e
marshki <marshall@morethantomorrow.org>
2019-12-31 11:45:29 -0500

    works! need to add expected failures

--- tests/test.py
+++ tests/test.py
@@ -1,9 +1,11 @@
 """Seed values to run simulation.
 For reference:
-      - rock beats scissors
-      - scissors beats paper
-      - paper beats rock.
+  - rock beats scissors
+  - scissors beats paper
+  - paper beats rock.
 """
+
+import unittest
 
 HUMAN = ['r', 'p', 's', 's', 's', 'r', 'p']
 BOT = ['s', 'r', 'p', 's', 'r', 'p', 's']
@@ -12,9 +14,26 @@ WINNING_COMBOS = [('r', 's'), ('p', 'r'), ('s', 'p')]
 
 ZIPPED = zip(HUMAN, BOT)
 
-for _i in ZIPPED:
-    print(_i)
+def evaluate_round(_i):
+    """Placeholder.
+    """
     if _i in WINNING_COMBOS:
-        print('True')
-    else: 
-        print('False')
+        return True
+    else:
+        return False
+
+#for _i in ZIPPED:
+#    evaluate_round(_i)
+
+class EvaluateRound(unittest.TestCase):
+    """Unit tests.
+    """
+    def test_evaluate_round(self):
+        """Placeholder.
+        """
+        for _i in ZIPPED:
+            with self.subTest(_i=_i):
+                self.assertTrue(evaluate_round(_i))
+
+if __name__ == '__main__':
+    unittest.main()
