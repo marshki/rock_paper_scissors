b005805efe3d05583e774757a45757d1390e950f
marshki <marshall@morethantomorrow.org>
2020-01-01 10:25:34 -0500

    add test to check for false assertions

--- tests/test.py
+++ tests/test.py
@@ -7,12 +7,16 @@ For reference:
 
 import unittest
 
-HUMAN = ['r', 'p', 's', 's', 's', 'r', 'p']
-BOT = ['s', 'r', 'p', 's', 'r', 'p', 's']
+HUMAN_ROUND1 = ['r', 'p', 's']
+BOT_ROUND1 = ['s', 'r', 'p']
+
+HUMAN_ROUND2 = ['s', 's', 'r', 'p']
+BOT_ROUND2 = ['s', 'r', 'p', 's']
 
 WINNING_COMBOS = [('r', 's'), ('p', 'r'), ('s', 'p')]
 
-ZIPPED = zip(HUMAN, BOT)
+ZIPPED1 = zip(HUMAN_ROUND1, BOT_ROUND1)
+ZIPPED2 = zip(HUMAN_ROUND2, BOT_ROUND2)
 
 def evaluate_round(_i):
     """Placeholder.
@@ -22,18 +26,22 @@ def evaluate_round(_i):
     else:
         return False
 
-#for _i in ZIPPED:
-#    evaluate_round(_i)
-
 class EvaluateRound(unittest.TestCase):
     """Unit tests.
     """
-    def test_evaluate_round(self):
+    def test_evaluate_round1(self):
         """Placeholder.
         """
-        for _i in ZIPPED:
+        for _i in ZIPPED1:
             with self.subTest(_i=_i):
                 self.assertTrue(evaluate_round(_i))
+
+    def test_evaluate_round2(self):
+        """Placeholder.
+        """
+        for _i in ZIPPED2:
+            with self.subTest(_i=_i):
+                self.assertFalse(evaluate_round(_i))
 
 if __name__ == '__main__':
     unittest.main()
