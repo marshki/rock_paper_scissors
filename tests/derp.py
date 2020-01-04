239180f48eb2821409003e56413c883eddcb8e41
marshki <marshall@morethantomorrow.org>
2020-01-04 12:25:14 -0500

    works

--- tests/derp.py
+++ tests/derp.py
@@ -1,17 +1,36 @@
 """Placeholder.
 """
 
-HUMAN_MOVE = ['r', 'p', 's', 's', 's', 'r', 'p', 'r', 's']
-BOT_MOVE = ['s', 'r', 'p', 's', 'r', 'p', 's', 's', 'p']
+import unittest
+
+HUMAN_MOVE = ['r', 'p', 's', 's', 's']
+BOT_MOVE = ['s', 's', 'p', 's', 'r']
+
 ROUND = zip(HUMAN_MOVE, BOT_MOVE)
 
 WINNING_COMBOS = [('r', 's'), ('p', 'r'), ('s', 'p')]
+
+EXPECTED_RESULT = [True, False, True, False, False]
 RESULT = []
 
-for _i in ROUND:
-    if _i in WINNING_COMBOS:
-        RESULT.append(bool(_i))
-    else:
-        RESULT.append(bool())
+def evaluate_round():
+    """Placeholder.
+    """
+    for _i in ROUND:
+        if _i in WINNING_COMBOS:
+            RESULT.append(bool(_i))
+        else:
+            RESULT.append(bool())
 
-print(RESULT)
+evaluate_round()
+
+class EvaluateRound(unittest.TestCase):
+    """Placeholder.
+    """
+    def test_evaluate_round(self):
+        """Placeholder.
+        """
+        self.assertListEqual(RESULT, EXPECTED_RESULT)
+
+if __name__ == '__main__':
+    unittest.main()
