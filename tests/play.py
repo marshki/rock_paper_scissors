e52328b9a5bad4bedbc859449ef56825b866807a
marshki <marshall@morethantomorrow.org>
2020-01-05 11:15:30 -0500

    update for testing

--- tests/play.py
+++ tests/play.py
@@ -3,19 +3,34 @@
 """Unit testing class.
 """
 
+GAME_LENGTH = 1
+SCORES = {'human':0, 'bot':0}
+
+RESULT = 'bot'
+
 def play():
     """Placeholder.
     """
+    while True:
+        SCORES[RESULT] += 1
+        print("Your score: {human}, Bot score: {bot}".format(**SCORES))
+
+        if SCORES['bot'] == GAME_LENGTH:
+            print("\nThe bot wins, you puny human. ")
+            break
+
+        elif SCORES['human'] == GAME_LENGTH:
+            print("\nThe puny human wins.")
+            break
+play()
 
 """There are two items that need to be tested:
-- does the score counter work?  
-- does the game end when it should?
-"""
+- does the score counter work? (yes)
+- does the game end when it should? (yes, but we need to run a simulation of the while loop)
 
 class_Play(unitest.TestCase):
-    """Unit test.
-    """
+    Unit test.
 
     def test_play():
-        """Placeholder.
-        """
+        Placeholder.
+"""
