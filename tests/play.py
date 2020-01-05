6e5c48d9194d01b211f4a879b9bbf56436d7313c
marshki <marshall@morethantomorrow.org>
2020-01-04 23:25:02 -0500

    add as placeholder

---
+++ tests/play.py
@@ -0,0 +1,21 @@
+#!/usr/bin env python
+
+"""Unit testing class.
+"""
+
+def play():
+    """Placeholder.
+    """
+
+"""There are two items that need to be tested:
+- does the score counter work?  
+- does the game end when it should?
+"""
+
+class_Play(unitest.TestCase):
+    """Unit test.
+    """
+
+    def test_play():
+        """Placeholder.
+        """
