803d1a058f5bfd08db03010ffdbcc3bd3040640b
marshki <marshall@morethantomorrow.org>
2020-01-09 11:07:24 -0500

    rename to see if that fixes CI fails

---
+++ tests/test_player1.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# pylint: disable=W0613,W0622
+
+"""Unit testing class.
+"""
+
+import unittest
+from unittest.mock import patch
+
+MOVES = ["r", "p", "s"]
+
+def player1():
+    """Prompt user for input, and limit options to acceptable moves.
+    """
+
+    valid_input = MOVES + ['q']
+
+    while True:
+
+        player1_move = input("Enter your choice: ").lower()
+
+        if player1_move not in valid_input:
+            print("Sorry, that\'s invalid input. Try again: ")
+
+        else:
+            return player1_move
+
+class Player1(unittest.TestCase):
+    """Unit tests.
+    Use `patch()` to mock objects for testing.
+    For reference: https://docs.python.org/3/library/unittest.mock.html
+    """
+
+    @patch('builtins.input', return_value='r')
+    def test_player1_01(self, input):
+        """Valid return value.
+        """
+        self.assertIn(player1(), ['r', 'p', 's', 'q'])
+
+    @patch('builtins.input', return_value='q')
+    def test_player1_02(self, input):
+        """Valid return value.
+        """
+        self.assertIn(player1(), ['r', 'p', 's', 'q'])
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_rounds.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# pylint: disable=W0613,W0622
+
+"""Unit testing class.
+"""
+
+import unittest
+from unittest.mock import patch
+
+def rounds():
+    """Prompt user.
+    Return number of rounds as integer.
+    """
+
+    while True:
+        try:
+            return int(input("Type the number of rounds to play and press enter: "))
+        except ValueError:
+            print("Sorry, that\'s invalid input. Try again: ")
+            raise
+
+class Rounds(unittest.TestCase):
+
+    """Unit tests.
+    Use `patch()` to mock objects for testing.
+    For reference: https://docs.python.org/3/library/unittest.mock.html
+    """
+
+    @patch('builtins.input', return_value='10')
+    def test_rounds_01(self, input):
+        """Valid return value.
+        """
+        self.assertIsInstance(rounds(), int)
+
+    @patch('builtins.input', return_value='Derp!')
+    def test_rounds_02(self, input):
+        """Invalid return value.
+        """
+        with self.assertRaises(ValueError):
+            rounds()
+
+if __name__ == '__main__':
+    unittest.main()
